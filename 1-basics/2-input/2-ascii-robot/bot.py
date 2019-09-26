a03627cc769409c73ea4b209322f0a95bbfcafa9
adam-worley <adam@worleynet.co.uk>
2019-09-26 11:47:34 +0000

    added code to demonstrate different data types

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,11 @@
+# User selects beep eyes
+print("please enter a character for the eye")
+eye = input()
+print("Beep's expression is now as follows")
+print()
+print("############")
+print("# (", eye, eye,  ") #")
+print("#(  ___  )#")
+print("############")
+
+
