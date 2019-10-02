ae8bd1efc26a6734b3f9b18af6dbdfdac932f5ca
adam-worley <50365867+adam-worley@users.noreply.github.com>
2019-10-02 10:02:26 +0100

    added code to demonstrate the use of multiple nested statements

--- 1-basics/3-decision/8-nestception/bot.py
+++ 1-basics/3-decision/8-nestception/bot.py
@@ -1,19 +1,25 @@
 print("where should I look")
 location_1 = input()
-if(location_1 == "bedroom"):
+if(location_1 == "in the bedroom"):
     print("where in the bedroom should I look")
     room_location_1 = input()
-    if(room_location_1 == "in the cupboard"):
-        print("found some mess but no battery")
-        print()
-        print("where should I look")
-        location_2 = input()
-        if(location_2 == "bedroom"):
-            print("I have already looked here, where should i look?")
-            location_2 = input()
+    if(room_location_1 == "under the bed"):
+        print("found some shoes but no battery")
     else:
-        print("I can't look there, where in the bedroom should I look?")
-       
-        
-
-
+        print("I found some mess but no battery ")
+elif(location_1 == "in the bathroom"):
+    print("where in the bathroom should I look")
+    room_location_1 = input()
+    if(room_location_1 == "in the bath"):
+        print("found a rubber duck but no battery")
+    else:
+        print("it is wet but I found no battery ")
+elif(location_1 == "in the lab"):
+    print("where in the lab should I look")
+    room_location_1 = input()
+    if(room_location_1 == "on the table"):
+        print("yes! i found my battery")
+    else:
+        print("found some tools but no battery")
+else:
+    print("I don't know where that is but I will keep looking")
