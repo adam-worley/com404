969f78895025e150309a2205e6f2ef5adb9ea798
adam-worley <50365867+adam-worley@users.noreply.github.com>
2019-10-14 12:05:29 +0100

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-print ("how many steps are we away from the cave?")
-n = int(input())
-print()
-for count in range(n, 0, -1):
-    n = n-1
-    print(n, "steps remaining")
-print()
-print("we have reached the cave!")
