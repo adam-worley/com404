0cc0c91d4420a940ffdf38ba0c7f77ce439932c9
adam-worley <50365867+adam-worley@users.noreply.github.com>
2019-10-15 14:56:25 +0100

    Added code to demonstrate the use of a user-defined function with multiple parameters.

---
+++ 1-basics/5-functions/5-multiple-parameters/bot.py
@@ -0,0 +1,8 @@
+def climb_ladder (steps_left,steps_crossed):
+    if(steps_left>steps_crossed):
+        print("still some way to go!")
+    else:
+        print("we made it!")
+
+climb_ladder(5,2)
+climb_ladder(5,5)
