13b6ebe0e4b194ff4a01bd5372c91b23b788a4a7
adam-worley <50365867+adam-worley@users.noreply.github.com>
2019-10-28 14:41:43 +0000

    minimum tca completed

---
+++ 1-basics/6-mocktca/1-minimumtca/Q2.py
@@ -0,0 +1,8 @@
+print("Where is Forky?")
+forky_location=str(input())
+if (forky_location=="With Bonnie"):
+    print("Phew! Bonnie will be happy.")
+elif(forky_location=="Running away"):
+    print ("Oh no! Bonnie will be upset!")
+else:
+    print("Ah! I better look for him")
---
+++ 1-basics/6-mocktca/1-minimumtca/Q3.py
@@ -0,0 +1,10 @@
+print("How many miles must I travel before I reach the secret base?")
+miles_left=int(input())
+print("I will count the miles...")
+print()
+while(miles_left>0):
+    print("I have",miles_left,"miles to go before I reach the base.")
+    miles_left=miles_left-1
+print()
+print("I have arriced at the secret headquarters of the MIB!")
+print("It is time to sneak in")
